590d7b1c0a1234e68a4410894c53fe736a7da86f
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2024-09-17 23:21:59 +0530

    added directory

---
+++ Server/visionaries/visionaries/src/main/java/com/sih/visionaries/VisionariesApplication.java
@@ -0,0 +1,13 @@
+package com.sih.visionaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionariesApplication.class, args);
+	}
+
+}
