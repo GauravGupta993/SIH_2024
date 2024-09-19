160bee6dc929ab3b8c89ac54a9f014bd115f4de2
sp9324 <headintheclouds104@gmail.com>
2024-09-19 09:10:57 +0530

    flask

--- Flask_Server/app.py
+++ Flask_Server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS, cross_origin
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from pypdf import PdfReader
@@ -6,6 +7,13 @@ import os
 
 # Initialize Flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+@cross_origin()
+def helloWorld():
+  print("Hello, cross-origin-world!")
 
 # Initialize the Sentence Transformer model using BERT with mean-tokens pooling
 model = SentenceTransformer('bert-base-nli-mean-tokens')
@@ -24,9 +32,9 @@ def calculate_similarity(text1, text2):
     similarity_scores = cosine_similarity([sentence_embeddings[0]], sentence_embeddings[1:])
     return similarity_scores[0][0]
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -41,19 +49,27 @@ def upload_file():
         # Save the file to a temporary location
         file_path = os.path.join('/tmp', file.filename)
         file.save(file_path)
+        print("got resume")
 
         # Extract text from the PDF
         resume_text = extract_text_from_pdf(file_path)
+        print("got resume text")
 
         # Example job description text
         job_description = request.form.get('job_description', '')
+        print("got jd")
 
         # Calculate similarity
         similarity_score = calculate_similarity(resume_text, job_description)
+        print("got score")
 
         # Remove the temporary file
         os.remove(file_path)
 
+        # Convert similarity score to a standard Python float
+        similarity_score = float(similarity_score)
+        print("corrected format of score")
+        
         return jsonify({"similarity_score": similarity_score}), 200
 
     return jsonify({"error": "Invalid file format"}), 400
